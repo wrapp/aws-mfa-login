8070d4c2c96c2bba3a8084b0bb4c940e0050ffdb
Fredrik Håård <fredrik@metallapan.se>
2018-01-18 11:10:13 +0100

    Cleanup and docs

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # aws-mfa-auth
 Command-line tool for MFA authentication against the AWS CLI
+
+Will by default ask for MFA token, and grab MFA device serial from the default profile in `~/.aws/config`.
+
+Profile, serial and token can also be passed on the command line.
+
+You must have a valid authentication for AWS CLI already set up for the STS call to succeed.
+
+Examples:
+
+```
+> aws-mfa-auth --help
+usage: aws-mfa-auth [-h] [--token TOKEN] [--profile PROFILE | --serial SERIAL]
+                    [--fish | --bash]
+
+optional arguments:
+  -h, --help         show this help message and exit
+  --token TOKEN      MFA token to use for login
+  --profile PROFILE  AWS Profile to read MFA Serial from
+  --serial SERIAL    MFA device serial (e.g.
+                     arn:aws:iam::00000000000:mfa/myiamuser)
+  --fish             Override shell guessing, set to fish (use "set -e")
+  --bash             Override shell guessing, set to bash (use "unset")
+
+> aws-mfa-auth 
+MFA token: 123456
+export AWS_ACCESS_KEY_ID=ASIAJ5YFB3RUUOXGYOZQ
+export AWS_SECRET_ACCESS_KEY=abWbgMq5432lLIn6x34tj+Wlpykq1WR/KvbG2SXg
+export AWS_SESSION_TOKEN=FQodyXlDvLv//////////wEaDCDCm3ZyUN0wDhSDKd3klfssZo4zNgTqnmUiVH0Hp8EUwtdKwvbiAa7JsyXVfzP2vaM0MTZmur/SDFDSf33/77WSdNtpUnaMyEnNP//XA7OVzmzlMLAXKYAbzrq3tBVuXxspEccz+qrxMZkfXD+DfLfkgbKF384kSDksKDF+85kZZTTr6t4t7v1tZ9DNV3xEehNJk8BS5yrD6vKusGRir+ZVm3SDFddfdsDFFD
+```
--- wrapp/aws/mfa.py
+++ wrapp/aws/mfa.py
@@ -1,5 +1,5 @@
 """
-
+CLI tool to generate environment variables from AWS STS get-session-token when MFA is needed for AWS CLI actions.
 """
 import json
 import subprocess
@@ -9,8 +9,8 @@ from os.path import expanduser
 import os
 import sys
 
-
-def main():
+def parse_args() -> argparse.Namespace:
+    """Parse arguments and generate help. Tries to guess if this is a fish shell."""
     parser = argparse.ArgumentParser()
     parser.add_argument('--token', type=int, default=None, help='MFA token to use for login')
 
@@ -19,11 +19,21 @@ def main():
     serial_group.add_argument('--serial', type=int, default=None, help='MFA device serial (e.g. arn:aws:iam::00000000000:mfa/myiamuser)')
 
     shell_group = parser.add_mutually_exclusive_group()
-    shell_group.add_argument('--fish', action='store_true')
-    shell_group.add_argument('--bash', action='store_true')
+    shell_group.add_argument('--fish', action='store_true', help='Override shell guessing, set to fish (use "set -e")')
+    shell_group.add_argument('--bash', action='store_true', help='Override shell guessing, set to bash (use "unset")')
 
-    args = parser.parse_args()
+    args =  parser.parse_args()
 
+    # Guess shell for unset
+    if not args.bash and (any([k for k in os.environ if k.startswith('__fish')])):
+        args.fish = True 
+
+    return args
+
+def get_credentials(args: argparse.Namespace) -> dict:
+    """Get the credentials from AWS STS.
+    If no serial number is given in the args, will try to read from `.aws/config` profile.
+    If no token is given, asks for it using `input()`"""
     if args.serial is None:
         config = configparser.ConfigParser()
         config.read(expanduser("~/.aws/config"))
@@ -45,10 +55,12 @@ def main():
     # AWS Session token already set but expired will make it break
     clean_env = os.environ.copy()
     if 'AWS_SESSION_TOKEN' in clean_env:
+        print("Deleting session token")
         del clean_env['AWS_SESSION_TOKEN']
 
+    # Get the session token
     proc = subprocess.Popen([
-        "aws",  "sts", "get-session-token",
+        "aws",  "--profile", args.profile, "sts", "get-session-token",
         "--serial-number", mfa_serial,
         "--token-code", mfa_token, '--output', 'json'
      ],stdout=subprocess.PIPE, stderr=subprocess.STDOUT, env=clean_env)
@@ -58,19 +70,26 @@ def main():
         sys.exit(-1)
 
     credentials = json.loads(data.decode('utf-8'))["Credentials"]
-    if args.bash or (not any([k for k in os.environ if k.startswith('__fish')]) and not args.fish):
-        export = 'export %s=%s'
-        unset = 'unset %s'
-    else:
-        export = 'set -x %s %s'
-        unset = 'set -e %s'
+    return credentials
 
+def print_credentials(credentials: dict, fish=False):
+    """Print export commands for the credentials dict given.
+    If AWS_PROFILE is set, also print unset command for it."""
+    export = 'export %s=%s' if not fish else 'set -x %s %s'
     print(export % ("AWS_ACCESS_KEY_ID", credentials["AccessKeyId"]))
     print(export % ("AWS_SECRET_ACCESS_KEY", credentials["SecretAccessKey"]))
     print(export % ("AWS_SESSION_TOKEN", credentials["SessionToken"]))
 
+    # Profile messes with the setup
     if 'AWS_PROFILE' in os.environ:
-        print(unset % 'AWS_PROFILE')
+        print('unset AWS_PROFILE' if not fish else 'set -e AWS_PROFILE' )
+
+
+def main():
+    """Parse arguments, get credentials, print credentials."""
+    args = parse_args()
+    credentials = get_credentials(args)
+    print_credentials(credentials, fish=args.fish)
 
 if __name__ == '__main__':
     main()
