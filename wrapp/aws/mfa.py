e04068aff8a8c9e45bf780170402c834f708dc83
Fredrik Håård <fredrik.haard@wrapp.com>
2018-05-08 12:25:54 +0200

    Fix type of token argument so leading zeros are not removed

--- wrapp/aws/mfa.py
+++ wrapp/aws/mfa.py
@@ -9,26 +9,32 @@ from os.path import expanduser
 import os
 import sys
 
+
 def parse_args() -> argparse.Namespace:
     """Parse arguments and generate help. Tries to guess if this is a fish shell."""
     parser = argparse.ArgumentParser()
-    parser.add_argument('--token', type=int, default=None, help='MFA token to use for login')
+    parser.add_argument('--token', type=str, default=None, help='MFA token to use for login')
 
     serial_group = parser.add_mutually_exclusive_group()
-    serial_group.add_argument('--profile', type=str, default='default', help='AWS Profile to read MFA Serial from')
-    serial_group.add_argument('--serial', type=int, default=None, help='MFA device serial (e.g. arn:aws:iam::00000000000:mfa/myiamuser)')
+    serial_group.add_argument('--profile', type=str, default='default',
+                              help='AWS Profile to read MFA Serial from')
+    serial_group.add_argument('--serial', type=int, default=None,
+                              help='MFA device serial (e.g. arn:aws:iam::00000000000:mfa/myiamuser)')
 
     shell_group = parser.add_mutually_exclusive_group()
-    shell_group.add_argument('--fish', action='store_true', help='Override shell guessing, set to fish (use "set -e")')
-    shell_group.add_argument('--bash', action='store_true', help='Override shell guessing, set to bash (use "unset")')
+    shell_group.add_argument('--fish', action='store_true',
+                             help='Override shell guessing, set to fish (use "set -e")')
+    shell_group.add_argument('--bash', action='store_true',
+                             help='Override shell guessing, set to bash (use "unset")')
 
-    args =  parser.parse_args()
+    args = parser.parse_args()
 
     # Guess shell for unset
     if not args.bash and (any([k for k in os.environ if k.startswith('__fish')])):
-        args.fish = True 
+        args.fish = True
 
     return args
+
 
 def get_credentials(args: argparse.Namespace) -> dict:
     """Get the credentials from AWS STS.
@@ -63,7 +69,7 @@ def get_credentials(args: argparse.Namespace) -> dict:
         "aws",  "--profile", args.profile, "sts", "get-session-token",
         "--serial-number", mfa_serial,
         "--token-code", mfa_token, '--output', 'json'
-     ],stdout=subprocess.PIPE, stderr=subprocess.STDOUT, env=clean_env)
+    ], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, env=clean_env)
     data, _ = proc.communicate()
     if proc.returncode != 0:
         print("# get-session-token failed: ", data.decode('utf-8'))
@@ -71,6 +77,7 @@ def get_credentials(args: argparse.Namespace) -> dict:
 
     credentials = json.loads(data.decode('utf-8'))["Credentials"]
     return credentials
+
 
 def print_credentials(credentials: dict, fish=False):
     """Print export commands for the credentials dict given.
@@ -82,7 +89,7 @@ def print_credentials(credentials: dict, fish=False):
 
     # Profile messes with the setup
     if 'AWS_PROFILE' in os.environ:
-        print('unset AWS_PROFILE' if not fish else 'set -e AWS_PROFILE' )
+        print('unset AWS_PROFILE' if not fish else 'set -e AWS_PROFILE')
 
 
 def main():
@@ -90,6 +97,7 @@ def main():
     args = parse_args()
     credentials = get_credentials(args)
     print_credentials(credentials, fish=args.fish)
+
 
 if __name__ == '__main__':
     main()
